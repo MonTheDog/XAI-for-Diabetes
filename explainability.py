e9bce532e747e93b5ddc69725dbb4be5b77d3641
MonTheDog <cate.party@gmail.com>
2024-06-21 16:52:32 +0200

    Started refactoring work on code

--- explainability.py
+++ explainability.py
@@ -24,6 +24,62 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.8f' % x)
 
 
+def load_models(models_path):
+    # Carichiamo i modelli
+    for model_name, model_path in models_path.items():
+        with open(model_path, "rb") as f:
+            models[model_name] = pickle.load(f)
+
+
+def get_row_to_explain(dataset, target_row_index):
+    # Otteniamo la riga target e la trasformiamo in un Dataframe di una sola riga, per poi rimuovere l'Outcome
+    row_to_explain = dataset.iloc[target_row_index]
+    row_to_explain_df = row_to_explain.to_frame().T
+    row_to_explain_df = row_to_explain_df.drop(["Outcome"], axis=1)
+
+    # Mostriamo la riga selezionata dall'utente
+    print("\n", "-*-" * 10, "Riga Selezionata", "-*-" * 10, "\n")
+    print(row_to_explain_df)
+
+    # Scaliamo la riga con lo scaler, in quanto i modelli sono stati addestrati sul dataset scalato
+    # Inoltre abbiamo bisogno sia della riga sotto forma di array numpy che di Dataframe, in quanto
+    # i vari algoritmi potrebbero richiedere o uno o l'altro.
+    scaled_row_array = scaler.transform(row_to_explain_df)
+    scaled_row_df = pd.DataFrame(scaled_row_array, columns=dataset.columns[:-1])
+    return scaled_row_array, scaled_row_df
+
+
+def has_diabetes(diabetes_value):
+    print("\n", "-*-" * 10, "Risultato effettivo", "-*-" * 10, "\n")
+    if diabetes_value == 0:
+        print("La persona non ha il diabete")
+    else:
+        print("La persona ha il diabete")
+
+
+def show_results(models):
+    print("\n", "-*-" * 10, "Risultati dei Modelli", "-*-" * 10, "\n")
+
+    predictions = dict()
+    for model in models.keys():
+
+        # Effettuiamo una distinzione con la rete neurale per il suo diverso funzionamento, infatti
+        # non vogliamo la probabilità, ma il valore della classe, e per evitare di flooddare la console
+        # andiamo a impsotare la verbosità a 0
+        if model == "Neural Network":
+            predictions[model] = models[model].predict(scaled_row_array, verbose=0)
+            predictions[model] = (predictions[model] > 0.5).astype(int)
+        else:
+            predictions[model] = models[model].predict(scaled_row_array)
+
+        # Mostriamo se ogni modello ha indovinato la previsione a fini di studio
+        if predictions[model] == diabetes_value:
+            print(f"Il modello {model} ha predetto correttamente")
+        else:
+            print(f"Il modello {model} ha predetto in modo errato")
+
+    return predictions
+
 
 if __name__ == '__main__':
 
@@ -35,55 +91,34 @@ if __name__ == '__main__':
     }
 
     # Carichiamo i modelli
-    for model_name, model_path in models.items():
-        with open(model_path, "rb") as f:
-            models[model_name] = pickle.load(f)
+    load_models(models)
 
     # Carichiamo lo scaler
     with open("scaler.pkl", "rb") as f:
         scaler = pickle.load(f)
 
-    # Carichiamo il dataset e prendiamone una riga
-    target_row = 1
-
+    # Carichiamo il dataset e selezioniamo l'indice della riga che vogliamo spiegare
+    target_row = 0
     diabetes_dataset = pd.read_csv("diabetes.csv")
-    X_scaled = scaler.transform(diabetes_dataset.drop(["Outcome"], axis=1))
-    y = diabetes_dataset["Outcome"]
-    row_to_explain = diabetes_dataset.iloc[target_row]
-    row_to_explain_df = row_to_explain.to_frame().T
-    row_to_explain_df = row_to_explain_df.drop(["Outcome"], axis=1)
-    print(row_to_explain_df)
-    scaled_row = scaler.transform(row_to_explain_df)
-    scaled_row_df = pd.DataFrame(scaled_row, columns=diabetes_dataset.columns[:-1])
-    print(scaled_row_df)
 
-    if y[target_row] == 0:
-        print("La persona non ha il diabete")
-    else:
-        print("La persona ha il diabete")
+    # Poichè abbiamo bisogno sia di un array numpy che del dataframe scalato li costruiamo
+    X_scaled = scaler.transform(diabetes_dataset.drop(["Outcome"], axis=1))
+    dataframe_scaled = pd.DataFrame(X_scaled, columns=diabetes_dataset.columns[:-1])
+
+    # Prendiamo la riga da spiegare, sotto forma di array e di dataframe
+    scaled_row_array, scaled_row_df = get_row_to_explain(diabetes_dataset, target_row)
+
+    # Mostriamo se la persona ha o meno il diabete a fini di studio
+    diabetes_value = diabetes_dataset["Outcome"][target_row]
+    has_diabetes(diabetes_value)
 
     # Creiamo un dataframe vuoto per inserire poi i valori di explainability
     explainability_score = pd.DataFrame(columns=diabetes_dataset.columns[:-1])
-    predictions = dict()
 
     # Effettuiamo la previsione per ogni modello
-    for model in models.keys():
+    predictions = show_results(models)
 
-        if model == "Neural Network":
-            predictions[model] = models[model].predict(scaled_row, verbose=0)
-        else:
-            predictions[model] = models[model].predict(scaled_row)
-
-        if model == "Neural Network":
-            predictions[model] = (predictions[model] > 0.5).astype(int)
-
-        is_prediction_correct = predictions[model] == y[target_row]
-
-        if is_prediction_correct:
-            print(f"Il modello {model} ha predetto correttamente")
-        else:
-            print(f"Il modello {model} ha predetto in modo errato")
-
+    # TODO da qui
     # Otteniamo l'explanation con SHAP
     for model in models.keys():
 
@@ -115,7 +150,7 @@ if __name__ == '__main__':
     for model in models.keys():
         explainer = lime.lime_tabular.LimeTabularExplainer(X_scaled, mode="regression")
 
-        explanation = explainer.explain_instance(scaled_row.flatten(), models[model].predict, num_features=8)
+        explanation = explainer.explain_instance(scaled_row_array.flatten(), models[model].predict, num_features=8)
 
         # Convertiamo gli indici nei nomi delle colonne
         feature_names = diabetes_dataset.columns[:-1]
@@ -140,9 +175,6 @@ if __name__ == '__main__':
 
         # Aggiungiamo la riga al dataframe
         explainability_score.loc[model + " LIME"] = sorted_row
-
-    # Ricostruiamo il dataframe ma scalato
-    dataframe_scaled = pd.DataFrame(X_scaled, columns=diabetes_dataset.columns[:-1])
 
     # Otteniamo l'explanation con CIU
     for model in models.keys():
@@ -172,10 +204,11 @@ if __name__ == '__main__':
 
         if model == "Neural Network":
             # Perché serve un array unidimensionale e la Neural Network restituisce un array bidimensionale
-            explanation = explainer.explain_instance(scaled_row.flatten(), flatten_prediction, threshold=0.95)
+            explanation = explainer.explain_instance(scaled_row_array.flatten(), flatten_prediction, threshold=0.95)
         else:
-            explanation = explainer.explain_instance(scaled_row.flatten(), models[model].predict, threshold=0.95)
+            explanation = explainer.explain_instance(scaled_row_array.flatten(), models[model].predict, threshold=0.95)
 
+        # TODO riscalare i risultati
         print(model)
         print('Anchor: %s' % (' AND '.join(explanation.names())))
         print('Precision: %.2f' % explanation.precision())
